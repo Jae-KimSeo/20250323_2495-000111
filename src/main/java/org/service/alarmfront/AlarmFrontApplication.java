34b920fbf29eb0524530f378eb55793e6a747c2c
Jae-KimSeo <seojaeinblank@gmail.com>
2025-03-21 17:02:40 +0900

    project setting

---
+++ src/main/java/org/service/alarmfront/AlarmFrontApplication.java
@@ -0,0 +1,13 @@
+package org.service.alarmfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/service/alarmfront/AlarmFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package org.service.alarmfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
